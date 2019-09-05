f411cd996f5e04ef3831427ec8d8ca370b8488d7
Rafiki <raphaelle.mp@gmail.com>
2019-09-04 20:07:33 -0400

    Initial Commit

---
+++ Simple Dual Rhythm Game/Assets/Scripts/Metronome.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour {
+    /*
+     2 1
+     3 2 
+     4 3
+     5 4
+
+        6 5
+        7 6
+        8 7
+        9 8
+
+        10 9 
+        11 10
+        12 11
+        13 12
+         
+         */
+
+    public int bpm;
+
+    [HideInInspector]
+    public float frequency;
+
+    private int counter = 0;
+
+    private float initialTime;
+
+    private AudioSource tickSound;
+    private AudioSource[] instrumentSounds;
+    private AudioSource[] sounds;
+
+    private float points = 0;
+    private float errorMargin = 0.2f;
+
+    private bool bipAllowed = true;
+    private int lastCounter = 1;
+
+    PlayersManager playersScript;
+
+    void Start() {
+        frequency = 60f / bpm;
+
+        sounds = GetComponents<AudioSource>();
+        tickSound = sounds[0];
+
+        instrumentSounds = new AudioSource[4];
+
+        for (int i = 0; i < 4; i++) {
+            instrumentSounds[i] = sounds[i + 1];
+        }
+
+        playersScript = GameObject.Find("PlayersManager").GetComponent<PlayersManager>();
+        initialTime = Time.time;
+    }
+
+    void tick() {
+        tickSound.Play();
+
+        if (counter != 0 && counter % 4 == 0) {
+            playersScript.changeCurrentPlayer();
+        }
+
+        counter++;
+    }
+
+    void Update() {
+        float timeSpent = Time.time - initialTime;
+        if (timeSpent >= frequency * (counter) + errorMargin) {
+            bipAllowed = true;
+        }
+
+        if (timeSpent >= frequency * counter + 1) {
+            tick();
+        }
+    }
+
+    bool isOnTime() {
+        float currentTime = Time.time - initialTime;
+        float validTime = frequency * counter;
+        //Debug.Log ("Guitar:" + currentTime + " " + validTime);
+        if (bipAllowed && (currentTime <= frequency * (counter - 1) + errorMargin)) {
+            lastCounter = counter;
+            bipAllowed = false;
+            return true;
+        }
+        else if (bipAllowed && currentTime >= validTime - errorMargin) {
+            lastCounter = counter + 1;
+        }
+        return false;
+    }
+
+    void makePoints() {
+        points++;
+        Debug.Log("Points: " + points);
+    }
+
+    public void PlayNote(int noteIndex) {
+        if (isOnTime()) {
+            makePoints();
+        }
+
+        Debug.Log("Note " + noteIndex);
+        instrumentSounds[noteIndex].Play();
+    }
+}
---
+++ Simple Dual Rhythm Game/Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    /*
+     In metronome: 
+        Change Player after 2 bars (ish)
+        Keep track of recorded inputs
+        
+        Put a margin (test by printing true and false)
+        Score system
+         */
+
+
+    Dictionary<int, KeyCode> Player1Inputs;
+    Dictionary<int, KeyCode> Player2Inputs;
+    Dictionary<int, KeyCode> currentInputs;
+    Metronome metronomeScript;
+
+    Player currentPlayer;
+    Player player1;
+    Player player2;
+
+    public class Player {
+        public Dictionary<int, KeyCode> playerInputs;
+
+        public Player(Dictionary<int, KeyCode> inputs) {
+            playerInputs = inputs;
+        }
+    }
+
+    void Awake(){
+        Player1Inputs = SetPlayerInputs(KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow);
+        Player2Inputs = SetPlayerInputs(KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D);
+        metronomeScript = GameObject.Find("Metronome").GetComponent<Metronome>();
+
+        player1 = new Player(Player1Inputs);
+        player2 = new Player(Player2Inputs);
+        currentPlayer = player1;
+        currentInputs = player1.playerInputs;
+    }
+
+    void Update(){
+        for (int i = 0; i < currentInputs.Count; i++) {
+            if (Input.GetKeyDown(currentInputs[i])) {
+                metronomeScript.PlayNote(i);
+                break;
+            }
+        }
+    }
+
+    Dictionary<int, KeyCode> SetPlayerInputs(KeyCode KeyCode1, KeyCode KeyCode2, KeyCode KeyCode3, KeyCode KeyCode4) {
+        Dictionary<int, KeyCode> playersInput = new Dictionary<int, KeyCode>();
+
+        playersInput[0] = KeyCode1;
+        playersInput[1] = KeyCode2;
+        playersInput[2] = KeyCode3;
+        playersInput[3] = KeyCode4;
+
+        return playersInput;
+    }
+
+    public void changeCurrentPlayer() {
+        /*if (currentPlayer == null) {
+            currentPlayer = player1
+        }
+        else*/ if (currentPlayer.Equals(player1)) {
+            currentPlayer = player2;
+        }
+        else {
+            currentPlayer = player1;
+        }
+        currentInputs = currentPlayer.playerInputs;
+    }
+}
