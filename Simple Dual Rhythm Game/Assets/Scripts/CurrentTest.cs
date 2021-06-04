426fec8e7377c6879720bc9eb5e649462e3b9d56
Raphaëll <raphaelle.mp@gmail.com>
2021-06-04 15:11:28 -0400

    idk

---
+++ Simple Dual Rhythm Game/Assets/Scripts/CurrentTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CurrentTest : MonoBehaviour
+{
+    List<Keyboard> keyboards = new List<Keyboard>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboards.Count == 0 || keyboard != keyboards[0]) {
+            keyboards.Add(keyboard);
+        }
+
+        if (keyboard != null) {
+            Debug.Log(keyboards.Count);
+        }
+        
+    }
+}
